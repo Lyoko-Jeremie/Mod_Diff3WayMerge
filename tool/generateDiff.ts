c44687e1daf67d7c026333b0c759b327aa30c4d6
LyokoJeremie <lucheng989898@gmail.com>
2023-10-16 02:05:18 +0800

    tool

---
+++ tool/generateDiff.ts
@@ -0,0 +1,22 @@
+import {Diff, diff_match_patch} from "../src/diff_match_patch/diff_match_patch";
+import {DiffFormat} from "../src/DiffFormat";
+
+export function covertDiff2DiffFormat(d: Diff[]) {
+    return d.map<DiffFormat>(T => {
+        return {
+            op: T[0],
+            text: T[1],
+        };
+    })
+}
+
+export function generateDiff(
+    dmp: diff_match_patch,
+    modBase: string,
+    modDiff: string,
+): DiffFormat[] {
+    const d02 = dmp.diff_main(modBase, modDiff);
+    return covertDiff2DiffFormat(d02);
+}
+
+
